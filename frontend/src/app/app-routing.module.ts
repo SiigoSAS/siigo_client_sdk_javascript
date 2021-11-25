3b3ab0ed0dd85370f0a85d116ea01a67339adb9f
= <gome801557@siigo.com>
2021-11-25 14:39:17 -0500

    feat: addcomponentsfront

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AuthGuard } from "@core/guards";
+import { CreateCustomerComponent } from './feature/customer/create-customer/create-customer.component';
+import { CreateInvoiceComponent } from './feature/invoice/create-invoice/create-invoice.component';
+import { CreateProductComponent } from './feature/product/create-product/create-product.component';
 
 const routes: Routes = [
   {
@@ -11,7 +14,26 @@ const routes: Routes = [
     path: "contacts",
     canActivate: [AuthGuard],
     loadChildren: () => import("./feature/contact/contact.module").then((module) => module.ContactModule)
-  }
+  },
+  {
+    path:'create-customer',
+    component:
+    CreateCustomerComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'create-invoice',
+    component:
+    CreateInvoiceComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'create-product',
+    component:
+    CreateProductComponent,
+    pathMatch: 'full'
+  },
+
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,9 +7,13 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "@core/core.module";
+import { CreateInvoiceComponent } from './feature/invoice/create-invoice/create-invoice.component';
+import { AuthComponent } from './feature/auth/auth.component';
+import { CreateCustomerComponent } from './feature/customer/create-customer/create-customer.component';
+import { CreateProductComponent } from './feature/product/create-product/create-product.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CreateInvoiceComponent, AuthComponent, CreateCustomerComponent, CreateProductComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: "serverApp" }),
     AppRoutingModule,
