c931bdb2bca3f38a5d72951a8ae8765b40aeb8fd
kkxxz <card800398@siigo.com>
2021-12-01 21:34:32 +0000

    fix: apply standard and refact

--- api/controllers/customer.controller.js
+++ api/controllers/customer.controller.js
@@ -1,73 +1,73 @@
 // customerController.js
 // Import user model
-var SiigoInstance = require("./auth.controller");
+const SiigoInstance = require('./auth.controller')
 // Handle index actions
-const environment = require("../config/environment");
+const environment = require('../config/environment')
 
 exports.getCustomer = async (req, res) => {
-  let data = {};
-  if(req.params.id == undefined) {
+  let data = {}
+  if (req.params.id == undefined) {
     try {
-      let apiInstance = new SiigoInstance.SiigoApi.CustomerApi();
-      let opts = {
-        'identification': req.body.identification,
-        'branchOffice': req.body.branchOffice,
-        'active': req.body.active,
-        'type': req.body.type,
-        'personType': req.body.personType,
-        'createdStart': req.body.createdStart,
-        'createdEnd': req.body.createdEnd,
-        'dateStart': req.body.dateStart,
-        'dateEnd': req.body.createdEnd,
-        'updatedStart': req.body.updatedStart,
-        'updatedEnd': req.body.updatedEnd,
-        'page': req.body.page,
-        'pageSize': req.body.pageSize,
-      };
-  
-      data = await apiInstance.getCustomers(opts);
-      res.status(200).json( data );
+      const apiInstance = new SiigoInstance.SiigoApi.CustomerApi()
+      const opts = {
+        identification: req.body.identification,
+        branchOffice: req.body.branchOffice,
+        active: req.body.active,
+        type: req.body.type,
+        personType: req.body.personType,
+        createdStart: req.body.createdStart,
+        createdEnd: req.body.createdEnd,
+        dateStart: req.body.dateStart,
+        dateEnd: req.body.createdEnd,
+        updatedStart: req.body.updatedStart,
+        updatedEnd: req.body.updatedEnd,
+        page: req.body.page,
+        pageSize: req.body.pageSize
+      }
+
+      data = await apiInstance.getCustomers(opts)
+      res.status(200).json(data)
     } catch (error) {
-      res.json({ 
-        status: "Error",
-        message: "Something was wrong",
+      res.json({
+        status: 'Error',
+        message: 'Something was wrong',
         error: error
-      });
+      })
     }
   } else {
     try {
-      let apiInstance = new SiigoInstance.SiigoApi.CustomerApi();
-      let id = req.params.id; 
-  
-      data = await apiInstance.getCustomer(id);
-      res.status(200).json( data );
+      const apiInstance = new SiigoInstance.SiigoApi.CustomerApi()
+      const id = req.params.id
+
+      data = await apiInstance.getCustomer(id)
+      res.status(200).json(data)
     } catch (error) {
-      res.json({ 
-        status: "Error",
-        message: "Something was wrong",
+      res.json({
+        status: 'Error',
+        message: 'Something was wrong',
         error: error
-      });
+      })
     }
   }
-};
+}
 
 exports.createCustomer = async (req, res) => {
   res.status(200).json({
-    status: "success",
-    message: "createCustomer works!"
-  });
-};
+    status: 'success',
+    message: 'createCustomer works!'
+  })
+}
 
 exports.updateCustomer = async (req, res) => {
   res.status(200).json({
-    status: "success",
-    message: "updateCustomer works!"
-  });
-};
+    status: 'success',
+    message: 'updateCustomer works!'
+  })
+}
 
 exports.deleteCustomer = async (req, res) => {
   res.status(200).json({
-    status: "success",
-    message: "deleteCustomer works!"
-  });
-};
+    status: 'success',
+    message: 'deleteCustomer works!'
+  })
+}
--- api/controllers/document-type.controller.js
+++ api/controllers/document-type.controller.js
@@ -1,14 +1,16 @@
-// taxController.js
+// documentTypeController.js
 // Import user model
 const SiigoInstance = require('./auth.controller')
 // Handle index actions
 const environment = require('../config/environment')
 
-exports.getTaxes = async (req, res) => {
+exports.getDocumentTypes = async (req, res) => {
   try {
-    const apiInstance = new SiigoInstance.SiigoApi.TaxesApi()
-
-    const data = await apiInstance.getTaxes()
+    const apiInstance = new SiigoInstance.SiigoApi.DocumentTypeApi()
+    const opts = {
+      type: req.params.type
+    }
+    const data = await apiInstance.getDocumentTypes(opts)
     res.status(200).json(data)
   } catch (error) {
     res.json({
