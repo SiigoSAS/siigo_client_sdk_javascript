d298b4e75e4c934686d02b8a34cd4fcab7797e04
Gustavo Adolfo Moreno Muñoz <more802084@siigo.com>
2021-12-02 09:58:44 -0500

    fix(invoices): pagination in invoices list is corrected

--- frontend/src/app/feature/invoice/list-invoices/list-invoices.component.ts
+++ frontend/src/app/feature/invoice/list-invoices/list-invoices.component.ts
@@ -1,64 +1,66 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
-import { InvoiceService } from 'src/app/services/invoice.service';
-import { Router } from '@angular/router';
-import {MatTableDataSource} from '@angular/material/table';
-import { CustomerService } from '../../../services/customer.service';
-import {MatPaginator} from '@angular/material/paginator';
-
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { InvoiceService } from "src/app/services/invoice.service";
+import { Router } from "@angular/router";
+import { MatTableDataSource } from "@angular/material/table";
+import { CustomerService } from "../../../services/customer.service";
+import { MatPaginator } from "@angular/material/paginator";
+import { catchError, map, switchMap, tap } from "rxjs/operators";
+import { forkJoin, of } from "rxjs";
 
 @Component({
-  selector: 'app-list-invoices',
-  templateUrl: './list-invoices.component.html',
-  styleUrls: ['./list-invoices.component.scss']
-  
+  selector: "app-list-invoices",
+  templateUrl: "./list-invoices.component.html",
+  styleUrls: ["./list-invoices.component.scss"]
 })
 export class ListInvoicesComponent implements OnInit {
-  listInvoice:any;
-  displayedColumns: string[] = ['paymentTypes', 'name', 'date', 'identification','customername','total'];
-  dataSource : any=[];
-  opts:any={page:1,pageSize:10};
-  
-  
+  listInvoice: any;
+  displayedColumns: string[] = ["paymentTypes", "name", "date", "identification", "customername", "total"];
+  dataSource = new MatTableDataSource();
+  opts: any = { page: 1, pageSize: 10 };
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
     console.log(this.dataSource);
-    
   }
-  
+
   @ViewChild(MatPaginator) paginator: MatPaginator;
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
 
-
-  constructor(private _invoiceService : InvoiceService,private _customerService:CustomerService, private _router:Router) { 
-    this.listInvoice={}
+  constructor(private _invoiceService: InvoiceService, private _customerService: CustomerService, private _router: Router) {
+    this.listInvoice = {};
   }
 
   ngOnInit(): void {
     this.chargeInvoice();
   }
 
-  chargeInvoice(){
-    this._invoiceService.getInvoices(this.opts).subscribe(
-      (res)=>{      
-        this.dataSource = res['results'] ;
-        for (let index = 0; index < this.dataSource.length; index++) {
-          this._customerService.getCustomer(this.dataSource[index].customer.id).subscribe(
-            (res)=>{
-              this.dataSource[index].customer.name=res.name[0];
-                          
-            },(err)=>{
-              console.log(err);            
-            }
-          )                
-        }              
-      },
-      (err)=>{
-        console.log(err);        
-      }
-    )
+  chargeInvoice() {
+    this._invoiceService
+      .getInvoices(this.opts)
+      .pipe(
+        switchMap((res: any) => {
+          const { results } = res;
+          const array = results.map((item) => {
+            return this._customerService.getCustomer(item.customer.id).pipe(
+              map((r) => {
+                const name = r.name[0];
+                return { ...item, customer: { ...item.customer, name } };
+              })
+            );
+          });
+          return forkJoin(array);
+        }),
+        tap(console.log),
+        catchError((error) => {
+          console.log(error);
+          return of([]);
+        })
+      )
+      .subscribe((res) => {
+        this.dataSource.data = res;
+      });
   }
-
 }
