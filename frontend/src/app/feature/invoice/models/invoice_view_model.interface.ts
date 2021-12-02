87d40c77f797d70020b53648500e24b67cf923fb
Gustavo Adolfo Moreno Muñoz <more802084@siigo.com>
2021-12-02 10:45:08 -0500

    fix(invoices): search filter in invoices is fixed

---
+++ frontend/src/app/feature/invoice/models/invoice_view_model.interface.ts
@@ -0,0 +1,8 @@
+export interface InvoiceViewModel{
+  paymentType: string;
+  documentName: string
+  date: string;
+  customerIdentification: string;
+  customerName: string;
+  totalPrice: string;
+}
