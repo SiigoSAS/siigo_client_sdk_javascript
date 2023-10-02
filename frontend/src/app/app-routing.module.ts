dd490f75c8f5bab0dae06870bf53ac6f492ea44e
Diego Restrepo <rest800746@siigo.com>
2023-10-02 16:59:50 -0500

    fix: resolve update errors to version 14

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -61,7 +61,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      initialNavigation: "enabled",
+      initialNavigation: "enabledNonBlocking",
       relativeLinkResolution: "legacy"
     })
   ],
